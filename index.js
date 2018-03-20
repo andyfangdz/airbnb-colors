6b443b253b84253a6cbd511576a6f93e459d2758
Andy Fang <andyfang.dz@gmail.com>
2018-03-19 18:30:50 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const airbnbColors = {
+    hackberry: '#7B0051',
+    kazan: '#007A87',
+    babu: '#00D1C1',
+    lima: '#8CE071',
+    beach: '#FFB400',
+    ebisu: '#FFAA91',
+    tirol: '#B4A76C',
+    foggy: '#9CA299',
+    hof: '#565A5C',
+};
+
+module.exports = airbnbColors;
